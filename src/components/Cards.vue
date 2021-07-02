fd6f73f6c9bb2e57ea90d626a0c2abc69f6cfa09
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-07-02 14:07:19 -0700

    created Cards componenet

---
+++ src/components/Cards.vue
@@ -0,0 +1,159 @@
+<template>
+  <b-container>
+      <div id="sticks" class="card">
+          <h3>Sticks Combination</h3>
+          <p>Jab, rear upper-cut , lead hook, rear body-kick</p>
+          <br>
+          <h3>Advanced Sticks Combination</h3>
+          <p>Switch-jab, rear upper-cut, lead hook, rear body-kick</p>
+          <br>
+          <h3>Sticks Combination Counter - One Variation</h3>
+          <p>Lead inside-kick, rear over-head, lead body, rear kick (low, body, or head)</p>
+          <br>
+          <h3>Stick Combination Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=uXJoA42aPuk&t=30s">Reviewing the Sticks Combo and Countering</a>
+      </div>
+      <hr>
+      <div id="holland" class="card">
+          <h3>Holland Combination</h3>
+          <p>Jab, cross, lead hook</p>
+          <p>Reload for lead body, reload for lead hook, rear low kick</p>
+          <br>
+          <h3>Holland Combination Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=uadMkjatsd0&list=PLhKjAOsodBLmddG0i5fakLyWCWOKVvnjg&index=3&t=33s">Kickboxing For MMA: Holland Combination</a>
+          <a href="https://www.youtube.com/watch?v=RXi2E74K19I">BMT Holland Drill</a>
+      </div>
+      <hr>
+      <div id="hemmers" class="card">
+          <h3>Hemmers Combination</h3>
+          <p>Lead hook, rear hook, lead hook</p>
+          <p>Rear body, lead body</p>
+          <p>Lead hook, rear hook, lead hook</p>
+          <br>
+          <h3>Hemmers Combination Variation</h3>
+          <p>Lead hook, rear hook, lead hook</p>
+          <p>Rear body, lead body</p>
+          <p>Lead hook, rear hook, lead hook</p>
+          <p>Lead inside-kick, left body-kick</p>
+          <br>
+          <h3>Hemmers Combination Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=olzOj-21bfA">Kickboxing: Hemmers Combo With Coach Joey Banks & Duane Ludwig</a>
+      </div>
+      <hr>
+      <div id="dutch" class="card">
+          <h3>Dutch Drill #1</h3>
+          <p>Lead hook, rear low-kick</p>
+          <p>Cross, lead body-kick</p>
+          <p>Cross, lead hook, rear low-kick</p>
+          <p>Lead hook, cross, lead head-kick</p>
+          <br>
+          <h3>Dutch Drill #1 Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=aVZnAogUyVU&t=12s">The Dutch Drill</a>
+          <a href="https://www.youtube.com/watch?v=qO-vm9sXthM">How to teach the BMT Dutch drill</a>
+      </div>
+      <div id="230" class="card">
+          <h3>2:30</h3>
+          <p>Jab, cross</p>
+          <p>Jab, rear over-head</p>
+          <p>Jab, cross</p>
+          <p>Jab, rear hook</p>
+          <p>Jab, cross</p>
+          <p>Jab, rear upper-cut</p>
+          <br>
+          <h3>2:30 Variation</h3>
+          <p>Jab, cross, step back</p>
+          <p>Jab, jab, rear over-head</p>
+          <p>Lead low-kick</p>
+          <br>
+          <h3>2:30 Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=kOH7FYVtyWQ">The 2:30</a>
+          <a href="https://www.youtube.com/watch?v=IzmzEgNkAh8&list=PLhKjAOsodBLmddG0i5fakLyWCWOKVvnjg&index=17">Drilling 2:30 Series From Open Stance</a>
+      </div>
+      <div id="dekkers" class="card">
+          <h3>Dekkers Combination</h3>
+          <p>Jab, cross</p>
+          <p>Reload for cross, lead hook</p>
+          <p>Reload for lead hook, cross (drop on the cross a little)</p>
+          <p>Lead body, rear body</p>
+          <p>Reload for rear upper-cut, lead hook, cross</p>
+          <p>Two lead body-kicks</p>
+          <br>
+          <h3>New Dekkers Combination</h3>
+          <p>Jab, cross</p>
+          <p>Reload for cross, lead hook</p>
+          <p>Reload for lead hook, cross (drop on the cross a little)</p>
+          <p>Lead body, rear body</p>
+          <p>Reload for rear upper-cut, lead hook, cross</p>
+          <p>Lead inside-kick, lead high-kick</p>
+          <br>
+          <h3>Dekkers Combination Defensive Variations</h3>
+          <p>Combination is the same except with replacing reloads with slips, or rolls.</p>
+          <br>
+          <h3>Dekkers Combination Video Reference</h3>
+          <a href="https://www.youtube.com/watch?v=GclBhgtT8uQ&t=64s">KIckboxing For MMA: Dekkers Combination</a>
+      </div>
+      <div id="reem" class="card">
+              <h3>Reem Drill</h3>
+              <p>Cross, lead knee (step forward or switch), land forward after knee</p>
+              <p>Lead hook, cross</p>
+              <h3>Reem Setup Variation</h3>
+              <p>Lead hook, low-kick</p>
+              <p>Cross, lead knee (step forward or switch), land forward after knee</p>
+              <p>Lead hook, cross, lead hook</p>
+              <p>Rear high-kick</p>
+              <br>
+              <h3>Reem Drill Video Reference</h3>
+              <a href="https://www.youtube.com/watch?v=QbGz4xWVXTQ&t=36s">How To Drill Reems & Returns</a>
+              <a href="https://www.youtube.com/watch?v=-8PgdG8Vfs0">Cross Reem / Bang Muay Thai / Kickboxing For MMA / 2020</a>
+              <br>
+              <h3>Reem Setup Reference Video</h3>
+              <a href="https://www.youtube.com/watch?v=QmkDD6i3r4M">Reem Set Up / Kickboxing For MMA / 2020</a>
+          </div>
+          <div id="back-em-up" class="card">
+              <h3>Back Them Up Drill</h3>
+              <p>Jab, jab, cross</p>
+              <p>Jab, lead hook, cross</p>
+              <p>Lead hook, jab, cross</p>
+              <br>
+              <h3>Back Them Up Drill Video Reference</h3>
+              <a href="https://www.youtube.com/watch?v=9SX1jh4ATiI">Kickboxing For MMA: Back them up drill</a>
+              <a href="https://www.youtube.com/watch?v=2dagVVXpMHk">3 Different Ways to Use Combo #3 of the Back 'EM Up Drill</a>
+          </div>
+          <div id="ko" class="card">
+              <h3>K.O. Drill</h3>
+              <p>All punches are done while stepping back (which draws in your opponent)</p>
+              <p>Cross, cross</p>
+              <p>Rear over-head, rear over-head</p>
+              <p>Rear hook, rear hook</p>
+              <p>Rear upper-cut, rear upper-cut</p>
+              <br>
+              <h3>K.O. Drill Video Reference</h3>
+              <a href="https://www.youtube.com/watch?v=1DO4oQgdynI">The KO Drill With All Rear Punches</a>
+          </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+    name: 'Cards'
+}
+</script>
+
+<style scoped>
+    .card {
+        padding: 24px 10px;
+        margin-bottom: 24px;
+    }
+
+    h3 {
+        text-decoration: underline;
+    }
+
+    a {
+        color: #FF7B15;
+    }
+
+    a:hover {
+    color: #bd5b0f;
+  }
+</style>
