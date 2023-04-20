34d47a8549702e3649c38173a32d565bd3b275de
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-19 22:52:29 -0700

    installed chackra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+// import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
