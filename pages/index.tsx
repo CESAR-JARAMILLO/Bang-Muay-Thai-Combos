0f75471c8f0da1edf8b178b91e0f5beebc5d058c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-19 22:49:56 -0700

    added authentication check

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
+  const router = useRouter()
+  const isAuthenticated = document.cookie.includes('authenticated=true')
+
+  if (!isAuthenticated) {
+    router.push('/login')
+    return null
+  }
+
   return (
     <>
       <Head>
