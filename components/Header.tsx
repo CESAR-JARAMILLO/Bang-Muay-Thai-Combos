a702dffea7995b9f9eb712cbd9dc5d0ea42d87a3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-22 15:30:36 -0700

    Added content

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import {
+  Box,
+  VStack,
+  Image,
+  Heading,
+  Text,
+  Link,
+  HStack,
+  Button,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import bmtLogo from '../images/bmt.jpeg';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Box id="top" textAlign="center">
+      <Image src={bmtLogo.src} alt="BMT Logo" boxSize="100px" mx="auto" />
+      <Heading>BANG Muay Thai</Heading>
+      <Text fontSize="xl">Combos & Drills Reference</Text>
+      <Text>
+        All combos and drills are referenced from the BANG Muay Thai{" "}
+        <Link href="https://www.youtube.com/channel/UCYr42ia_GEP97beaCUXgbvw">
+          YouTube
+        </Link>{" "}
+        channel.
+      </Text>
+      <Text>
+        Some Variations are from the{" "}
+        <Link href="https://www.systemstrainingcenter.com/">
+          Systems Training Center
+        </Link>{" "}
+        classes
+      </Text>
+      <Text>
+        For any errors or suggestions feel free to contact me:{" "}
+        <Link href="mailto:csarjaramillo89@yahoo.com">
+          csarjaramillo89@yahoo.com
+        </Link>
+      </Text>
+      {/* <Button colorScheme="blue" href="belts.html">
+        Belt Requirements
+      </Button> */}
+    </Box>
   )
 }
 
