0b6da776808f41a4758110a81ba33a815976c761
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-24 15:16:37 -0700

    removed Card component and added HandCombos component

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
-import Card from './Card';
+// import Card from './Card';
+import HandCombos from './cards/HandCombos'
 import combos from '../combos';
 
 const CardSection = () => {
   return (
-    <Flex alignItems="center" justifyContent="center" h="100vh" bg="gray.100">
-      <Box maxW="md" w="full" p="5">
-        {combos.handCombos.map((handCombo, index) => (
-          <Card key={index} title={handCombo.title} combo={handCombo.combo} />
-        ))}
-        {combos.sticksCombos.map((sticksCombo, index) => (
-          <Card key={index} title={sticksCombo.title} combo={sticksCombo.combo} referenceLinks={sticksCombo.referenceLinks} />
-        ))}
-      </Box>
-    </Flex>
+    <HandCombos />
   );
 };
 
