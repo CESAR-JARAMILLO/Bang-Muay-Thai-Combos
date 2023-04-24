9600700b948d39a5627187d8277114ad1e00e81c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-23 18:23:18 -0700

    added Hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Box, Container, Heading, Text, Button, Image } from '@chakra-ui/react';
+import logo from '../assets/images/bmt.jpeg';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <Box position="relative">
+      <Image src={logo} mx="auto" display="block" mt="5" />
+      <Container maxW="container.lg" zIndex="1" position="relative">
+        <Heading as="h1" fontSize="6xl" textAlign="center" mt="10">
+          BANG Muay Thai
+        </Heading>
+        <Text fontSize="xl" textAlign="center" mt="3">
+          <Text fontWeight="bold" fontSize="3xl" mb="3">
+            Combos & Drills Reference
+          </Text>{' '}
+          All combos and drills are referenced from the BANG Muay Thai YouTube channel. Some variations are from the Systems Training Center classes.{' '}
+          For any errors or suggestions, feel free to contact me at: csarjaramillo89@yahoo.com.
+        </Text>
+        <Box textAlign="center" mt="10">
+          <Button colorScheme="blue" size="lg">
+            Learn more
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 
-export default Hero
+export default Hero;
