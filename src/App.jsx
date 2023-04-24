cf719c20e8ac76a8b3f932851dd2591d66e99f1c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-24 01:50:12 -0700

    added card section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Hero from './components/Hero'
 import QuickLinks from './components/QuickLinks'
+import CardSection from './components/CardSection'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Hero />
      <QuickLinks />
+     <CardSection />
     </>
   )
 }
