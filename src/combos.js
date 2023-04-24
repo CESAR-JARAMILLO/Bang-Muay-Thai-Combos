0c639f5bede3ee0e5fde51344dd6b37244d9c575
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-24 01:49:19 -0700

    created combos data file

---
+++ src/combos.js
@@ -0,0 +1,52 @@
+const combos = {
+  handCombos: [
+    {
+      title: "#'s 1-5 Hands",
+      combo: [
+        "#1 jab",
+        "#2 jab, cross",
+        "#3 jab, cross, lead hook",
+        "#4 jab, cross, lead hook, cross",
+        "#5 jab, cross, lead upper-cut, cross, lead hook",
+      ]
+    },
+    {
+      title: "#'s 6-11 Hands",
+      combo: [
+        "#6 cross, lead hook, cross",
+        "#7 lead hook, cross, lead hook",
+        "#8 rear upper-cut, lead hook, cross",
+        "#9 lead upper-cut, cross, lead hook",
+        "#10 cross, lead hook, rear upper-cut",
+        "#11 lead hook, cross, lead upper-cut",
+      ]
+    }
+  ],
+
+  sticksCombos: [
+    {
+      title: "Sticks Combination",
+      combo: [
+        "Jab, rear upper-cut, lead hook, rear body-kick"
+      ]
+    },
+    {
+      title: "Advance Sticks Combination",
+      combo: [
+        "Switch-jab, rear upper-cut, lead hook, rear body-kick"
+      ]
+    },
+    {
+      title: "Sticks Combination Counter - One Variation",
+      combo: [
+        "Lead inside-kick, rear over-head, lead body, rear kick (low, body, or head)"
+      ]
+    },
+    {
+      referenceTitle: "Reviewing the Sticks Combo and Countering",
+      referenceLinks: "https://www.youtube.com/watch?v=uXJoA42aPuk&t=30s"
+    }
+  ],  
+};
+
+export default combos;
