426606b7a7f2d357befc4f7e6adaa2f7cdfe54c4
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-25 12:15:47 -0700

    updated Heading

--- src/components/cards/HemmersCombos.jsx
+++ src/components/cards/HemmersCombos.jsx
@@ -8,7 +8,7 @@ const HemmersCombos = () => {
   return (
     <Card borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
       <CardHeader bg="blue.600" borderBottomWidth="1px">
-        <Heading size='md' color="white">Sticks Combinations</Heading>
+        <Heading size='md' color="white">Hemmers Combinations</Heading>
       </CardHeader>
 
       <CardBody>
--- src/components/cards/HollandCombos.jsx
+++ src/components/cards/HollandCombos.jsx
@@ -8,7 +8,7 @@ const HollandCombos = () => {
   return (
     <Card borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
       <CardHeader bg="blue.600" borderBottomWidth="1px">
-        <Heading size='md' color="white">Sticks Combinations</Heading>
+        <Heading size='md' color="white">Holland Combinations</Heading>
       </CardHeader>
 
       <CardBody>
