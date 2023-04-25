3baad849b344000a7427f1a965e9cdb5aa647818
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-25 11:38:36 -0700

    created styled and added HollandCombos component

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -3,6 +3,7 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 // import Card from './Card';
 import HandCombos from './cards/HandCombos'
 import SticksCombos from './cards/SticksCombos'
+import HollandCombos from './cards/HollandCombos'
 import combos from '../combos';
 
 const CardSection = () => {
@@ -10,6 +11,7 @@ const CardSection = () => {
     <>
       <HandCombos />
       <SticksCombos />
+      <HollandCombos />
     </>
   );
 };
---
+++ src/components/cards/HollandCombos.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Box, Text, Card, CardBody, CardHeader, Heading, Stack, StackDivider, Link } from "@chakra-ui/react";
+import combos from '../../combos';
+
+const HollandCombos = () => {
+  const hollandCombos = combos.hollandCombos;
+
+  return (
+    <Card borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
+      <CardHeader bg="blue.600" borderBottomWidth="1px">
+        <Heading size='md' color="white">Sticks Combinations</Heading>
+      </CardHeader>
+
+      <CardBody>
+      <Stack divider={<StackDivider />} spacing='4'>
+        <Box p="4">
+            <Heading size='sm' mb="2" textTransform='uppercase' fontWeight="bold" textDecoration="underline">
+              {hollandCombos[0].title}
+            </Heading>
+            {hollandCombos[0].combo.map((combo, index) => (
+              <Text key={index} fontSize='md'>
+                <span>{combo}</span>
+              </Text>
+            ))}
+          </Box>
+          {hollandCombos.slice(-1).map((item, index) => (
+            <Box key={index} p="4">
+              <Heading size='sm' mb="2" textTransform='uppercase' fontWeight="bold" textDecoration="underline">
+                {item.referenceHeading}
+              </Heading>
+              {item.links.map((link, index) => (
+                <Link key={index} href={link.referenceLinks}>{link.referenceTitle}</Link>
+              ))}
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default HollandCombos;
