8a7ffef47209f45846e81b6954c7605457779351
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-26 14:06:24 -0700

    updated id

--- src/components/cards/KODrillCombos.jsx
+++ src/components/cards/KODrillCombos.jsx
@@ -6,7 +6,7 @@ const KODrillCombos = () => {
   const koDrillCombos = combos.koDrillCombos;
 
   return (
-    <Card id='Hemmers' borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
+    <Card id='KO' borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
       <CardHeader bg="blue.600" borderBottomWidth="1px">
         <Heading size='md' color="white">K.O. Drill</Heading>
       </CardHeader>
