13b87b1cef8b5a7f0ad87bfb67dfd259c80686ae
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-25 19:59:13 -0700

    updated hero and added image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,65 @@
 import React from 'react';
-import { Box, Container, Heading, Text, Button, Image } from '@chakra-ui/react';
-import logo from '../assets/images/stc-logo.png';
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Flex,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import image from '../assets/images/hero.jpg';
 
 const Hero = () => {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
+
   return (
-    <Box position="relative">
-      <Image bg="blue.700" paddingLeft="4" paddingRight="4" src={logo} mx="auto" display="block" mt="5" />
+    <Box position="relative" height="60vh">
+      <Image
+        src={image}
+        alt="Hero"
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        objectFit="cover"
+        zIndex="-1"
+        height={isMobile ? '100%' : 'auto'}
+      />
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        bg="rgba(0, 0, 0, 0.6)"
+        zIndex="-1"
+      />
       <Container maxW="container.lg" zIndex="1" position="relative">
-        <Heading as="h1" fontSize="6xl" textAlign="center" mt="10">
-          BANG Muay Thai
-        </Heading>
-        <Text fontSize="xl" textAlign="center" mt="3">
-          <Text fontWeight="bold" fontSize="3xl" mb="3">
-            Combos & Drills Reference
-          </Text>{' '}
-          All combos and drills are referenced from the BANG Muay Thai YouTube channel. Some variations are from the Systems Training Center classes.{' '}
-          For any errors or suggestions, feel free to contact me at: csarjaramillo89@yahoo.com.
-        </Text>
-        <Box textAlign="center" mt="10">
-          <Button colorScheme="blue" size="lg">
-            Learn more
-          </Button>
-        </Box>
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          height="100%"
+          color="white"
+          alignItems="center"
+        >
+          <Heading as="h1" fontSize="6xl" textAlign="center" mt="10">
+            BANG Muay Thai
+          </Heading>
+          <Text fontSize="xl" textAlign="center" mt="3">
+            <Text fontWeight="bold" fontSize="3xl" mb="3">
+              Combos & Drills Reference
+            </Text>
+          </Text>
+          {/* <Box textAlign="center" mt="10">
+            <Button colorScheme="blue" size="lg">
+              Learn more
+            </Button>
+          </Box> */}
+        </Flex>
       </Container>
     </Box>
   );
