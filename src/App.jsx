14b15b93b5fd21d0f94244c38af02546d47b88a7
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-25 19:30:55 -0700

    added LoginPage and auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from 'react'
-import './App.css'
-import Hero from './components/Hero'
-import QuickLinks from './components/QuickLinks'
-import CardSection from './components/CardSection'
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import './App.css';
+import Hero from './components/Hero';
+import QuickLinks from './components/QuickLinks';
+import CardSection from './components/CardSection';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem('isAuthenticated');
+    if (isAuthenticated) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   return (
-    <>
-     <Hero />
-     <QuickLinks />
-     <CardSection />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/" element={!isAuthenticated ? <Navigate to="/login" /> : (
+          <>
+            <Hero />
+            <QuickLinks />
+            <CardSection />
+          </>
+        )} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
