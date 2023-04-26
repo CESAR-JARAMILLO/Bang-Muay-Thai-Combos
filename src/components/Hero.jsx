347c2d854968621a22b126365d21470996ce90d0
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-25 19:29:56 -0700

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Box, Container, Heading, Text, Button, Image } from '@chakra-ui/react';
-import logo from '../assets/images/bmt.jpeg';
+import logo from '../assets/images/stc-logo.png';
 
 const Hero = () => {
   return (
     <Box position="relative">
-      <Image src={logo} mx="auto" display="block" mt="5" />
+      <Image bg="blue.700" paddingLeft="4" paddingRight="4" src={logo} mx="auto" display="block" mt="5" />
       <Container maxW="container.lg" zIndex="1" position="relative">
         <Heading as="h1" fontSize="6xl" textAlign="center" mt="10">
           BANG Muay Thai
