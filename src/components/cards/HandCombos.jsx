4807329489f682b8e67205bee268eae32468964d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-03 16:09:28 -0700

    added card badge

--- src/components/cards/HandCombos.jsx
+++ src/components/cards/HandCombos.jsx
@@ -1,24 +1,75 @@
 import React from 'react';
-import { Box, Text, Card, CardBody, CardHeader, Heading, Stack, StackDivider } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Badge,
+} from '@chakra-ui/react';
 import combos from '../../combos';
 
 const HandCombos = () => {
   const handCombos = combos.handCombos;
 
   return (
-    <Card id='Hand-Combos' borderWidth="1px" borderRadius="lg" overflow="hidden" textAlign="center" maxW="sm" mx="auto">
+    <Card
+      id="Hand-Combos"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      textAlign="center"
+      maxW="sm"
+      mx="auto"
+    >
       <CardHeader bg="blue.600" borderBottomWidth="1px">
-        <Heading size='md' color="white">#'s 1-11 Hand Combos</Heading>
+        <Heading size="md" color="white">
+          #'s 1-11 Hand Combos
+        </Heading>
       </CardHeader>
 
       <CardBody>
-        <Stack divider={<StackDivider />} spacing='4'>
+        <Stack divider={<StackDivider />} spacing="4" mt="-4">
           {handCombos.map((combo, index) => (
             <Box key={index} p="4">
-              <Heading size='sm' mb="2" textTransform='uppercase' fontWeight="bold" textDecoration="underline">
+              {index === 0 ? (
+                <Badge
+                  colorScheme="white"
+                  variant="outline"
+                  border="1px"
+                  borderColor="black"
+                  mb="2"
+                  p="1"
+                  borderRadius="10"
+                >
+                  White Belt Requirement
+                </Badge>
+              ) : (
+                <Badge
+                  colorScheme="yellow"
+                  border="1px"
+                  borderColor="black"
+                  mb="2"
+                  p="1"
+                  borderRadius="10"
+                >
+                  Yellow Belt Requirement
+                </Badge>
+              )}
+
+              <Heading
+                size="sm"
+                mb="2"
+                textTransform="uppercase"
+                fontWeight="bold"
+                textDecoration="underline"
+              >
                 {combo.title}
               </Heading>
-              <Text fontSize='md'>
+              <Text fontSize="md">
                 {combo.combo.map((item, index) => (
                   <span key={index}>
                     {item}
